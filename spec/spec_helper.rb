e13dd87c17bf473dc87aa00851d313d170b53a98
Ronen Narkis <narkisr@gmail.com>
2013-06-09 02:21:02 +0300

    including apt

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,4 +5,5 @@ fixture_path = File.expand_path(File.join(__FILE__, '..', 'fixtures'))
 RSpec.configure do |c|
   c.module_path = '../:../modules'
   c.manifest_dir = File.join(fixture_path, 'manifests')
+  c.pre_condition = "include apt"
 end
