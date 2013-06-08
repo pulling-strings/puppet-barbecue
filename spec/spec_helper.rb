8832bab6ec5b93bfa657ac825980d19cf06762f0
Ronen Narkis <narkisr@gmail.com>
2013-06-09 02:11:12 +0300

    modules path fix

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,6 +3,6 @@ require 'rspec-puppet'
 fixture_path = File.expand_path(File.join(__FILE__, '..', 'fixtures'))
 
 RSpec.configure do |c|
-  c.module_path = '../:../../modules'
+  c.module_path = '../'
   c.manifest_dir = File.join(fixture_path, 'manifests')
 end
