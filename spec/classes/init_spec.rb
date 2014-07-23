09d865d17b60a6f0c3b6377ef056a5e39126dd70
narkisr <narkisr@gmail.com>
2014-07-23 23:53:01 +0300

    fixing specs

--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -3,11 +3,14 @@ require 'spec_helper'
 describe 'barbecue' do
   context "clean state" do
     let(:pre_condition) { "include apt"}
+    let(:facts) { {:osfamily=> 'Debian', :lsbdistid => 'debian'} }
+
     it { should contain_apt__key('celestial') }
     it { should contain_apt__source('barbecue') }
   end
 
   context "already defined" do
+   let(:facts) { {:osfamily=> 'Debian', :lsbdistid => 'debian'} }
    let(:pre_condition) { "apt::source { 'barbecue':release => 'quantal'}"}
    it { should_not contain_apt__key('celestial') }
   end
