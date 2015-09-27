874745f16ff4ccfe23863e15c03347f696f5a110
ronen <narkisr@gmail.com>
2015-09-27 17:43:47 +0300

    require apt update only for specific packages

--- manifests/packages.pp
+++ manifests/packages.pp
@@ -1,11 +1,7 @@
 # useful barbecue packages
 class barbecue::packages {
-
-  Class['apt::update'] -> Package<| |>
-
-  Apt::Source['barbecue'] ->
-
   package{['duply', 'nvm', 'supernal', 'fasd']:
-    ensure  => present
+    ensure  => present,
+    require => [Apt::Source['barbecue'], Class['apt::update']]
   }
 }
