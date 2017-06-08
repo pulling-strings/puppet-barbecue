5941941ffa7b22f8b1fa286dbe875c1617ea70a1
ronen <narkisr@gmail.com>
2017-06-08 21:15:10 +1000

    removing package

--- manifests/packages.pp
+++
@@ -1,7 +0,0 @@
-# useful barbecue packages
-class barbecue::packages {
-  package{['duply', 'supernal', 'fasd']:
-    ensure  => present,
-    require => [Apt::Source['barbecue'], Class['apt::update']]
-  }
-}
